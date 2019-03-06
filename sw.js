fb1e8dad42d4b4c275d0301ee4c9c650720062b6
Gerjohn IJzerman <gerjohn.ijzerman@ing.com>
2019-03-06 15:40:48 +0100

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,35 @@
 // registration of the serviceworker
+const CACHE_STATIC_NAME = 'static';
+const URLS_TO_PRECACHE = [
+    '/',
+    'index.html',
+    'src/js/app.js',
+    'src/js/feed.js',
+    'src/lib/material.min.js',
+    'src/lib/material.indigo-deep_orange.min.css',
+    'src/css/app.css',
+    'src/css/feed.css',
+    'src/images/main-image.jpg',
+    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    // 'https://code.getmdl.io/1.3.0/material.indigo-deep_orange.min.css"'
+];
+
+self.addEventListener('install', event => {
+    console.log('[Service Worker] Installing Service Worker ...', event);
+    event.waitUntil(
+        caches.open(CACHE_STATIC_NAME)
+            .then(cache => {
+                console.log('[Service Worker] Precaching App Shell');
+                cache.addAll(URLS_TO_PRECACHE);
+            })
+            .then(() => {
+                console.log('[ServiceWorker] Skip waiting on install');
+                return self.skipWaiting();
+            })
+    );
+});
+
 self.addEventListener('install', event => {
     console.log('[Service Worker] Installing Service Worker ...', event);
     event.waitUntil(self.skipWaiting());
@@ -17,5 +48,15 @@ self.addEventListener('fetch', event => {
         return;
     }
 
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    console.log(response);
+                    return response;
+                }
+
+                return fetch(event.request);
+            })
+    );
 });
